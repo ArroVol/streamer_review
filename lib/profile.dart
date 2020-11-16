68c166b964580c5fb1e73a68f1d9dd7c3d1db615
Brendamus <50035059+Brendamus@users.noreply.github.com>
2020-11-16 17:36:34 -0600

    Updated profile page and routing. Updated homepage formatting.

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,8 +1,71 @@
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        title: Text('User'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      backgroundColor: Colors.grey[500],
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/example.jpg'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'User Name',
+              style: TextStyle(color: Colors.blueGrey, letterSpacing: 2.0),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'ExampleUser1234',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 20),
+                Text(
+                  'test@gmail.com',
+                  style:TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18,
+                      letterSpacing: 1
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
