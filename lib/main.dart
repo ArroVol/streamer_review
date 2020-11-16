28f145969041e4cd4b949f180066f794414f3420
Brendamus <50035059+Brendamus@users.noreply.github.com>
2020-11-16 14:07:10 -0600

    Added login, registration, home

--- lib/main.dart
+++ lib/main.dart
@@ -2,117 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:streamer_review/secondScreen.dart';
 import 'package:streamer_review/widgets/home_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'login.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Streamer Review App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
 
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomePage()
-      // home: ColorCircle(title: 'Color Circle',),
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+  initialRoute: '/login',
+  routes: {
+    // '/': (context) => Loading(),
+    // '/home': (context) => Home(),
+    '/login': (context) => LoginScreen(),
+    // '/registration': (context) => Registration(),
+    // '/profile': (context) => Profile(),
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Widget titleSection = Container(
-
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
-        ],
-      ),
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-
-      ),
-      body: Center(
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .headline4,
-              ),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-
-    );
-  }
-}
-
+  },
+));
