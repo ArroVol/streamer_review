63a847cf69d356b687646ba573d0e4f94054fc1d
Aaron <arrovol47@gmail.com>
2020-11-23 15:04:14 -0600

    models and repositories completed.

--- lib/model/review.dart
+++ lib/model/review.dart
@@ -11,17 +11,18 @@ class Review {
   Review(
       {this.reviewId, this.satisfactionRating, this.entertainmentRating, this.interactivenessRating, this.broadcasterId, this.userId});
 
-  // Map<String, dynamic> toMap() {
-  //   return {'_id': id, 'email': email, 'password': password, 'phone_number': phoneNumber, 'user_name':userName};
-  // }
-  //
-  // Review.fromMap(Map<String, dynamic> m) {
-  //   id = m['_id'];
-  //   email = m['email'];
-  //   password = m['password'];
-  //   phoneNumber = m['phone_number'];
-  //   userName = m['user_name'];
-  // }
+  Map<String, dynamic> toMap() {
+    return {'reviews_id': reviewId, 'satisfaction_rating': satisfactionRating, 'entertainment_rating': entertainmentRating, 'interactiveness_rating': interactivenessRating, 'fk_broadcaster_id':broadcasterId , 'fk_user_id':userId};
+  }
+
+  Review.fromMap(Map<String, dynamic> m) {
+    reviewId = m['reviews_id'];
+    satisfactionRating = m['satisfaction_rating'];
+    entertainmentRating = m['entertainment_rating'];
+    interactivenessRating = m['interactiveness_rating'];
+    broadcasterId = m['fk_broadcaster_id'];
+    userId = m['fk_user_id'];
+  }
 
 // satisfaction_rating INTEGER,
 //     entertainment_rating INTEGER,
