07e6af56f485afb3ce5c446bd425836442909db8
Aaron <arrovol47@gmail.com>
2020-12-01 21:47:18 -0600

    swapped main classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:streamer_review/login.dart';
+import 'package:streamer_review/main_screen.dart';
 import 'package:streamer_review/profile.dart';
+import 'package:streamer_review/register.dart';
+import 'package:streamer_review/secure_storage/secure_storage.dart';
 
 
-import 'login.dart';
+final SecureStorage secureStorage = SecureStorage();
+final FlutterSecureStorage _secureStorage = FlutterSecureStorage();
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (secureStorage != null) {
+    String email = await secureStorage.readSecureData("email");
+    print(email);
+    if (email != null) {
+      print('passed if check, its not null');
+      print("***FOUND EMAIL***");
+      print("***$email***");
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/login',
+      // home: Profile();
+      runApp(MaterialApp(
+        initialRoute: '/main_screen',
+        routes: {
+          '/register': (context) => Register(),
+          '/login': (context) => LoginScreen(),
+          '/main_screen': (context) => MainScreen(),
+        },
+      ));
+    } else {
+      print("***THERE IS NOT AN EMAIL SAVED***");
+      // home: LoginScreen();
+      runApp(MaterialApp(
+        initialRoute: '/login',
+        routes: {
+          '/login': (context) => LoginScreen(),
+          '/register': (context) => Register(),
+          '/profile': (context) => Profile(),
+        },
+      ));
+    }
+  }
+}
 
-  routes: {
-    // '/': (context) => Loading(),
-    // '/home': (context) => Home(),
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Streamer Review App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginScreen(),
+      // home: ColorCircle(title: 'Color Circle',),
+    );
+  }
+}
 
-    '/login': (context) => LoginScreen(),
-    // '/login': (context) => MainScreen(),
-    // '/registration': (context) => Registration(),
-    '/profile': (context) => Profile(),
-
-
-
-  },
-));
+// runApp(MaterialApp(
+//   initialRoute: '/login',
+//
+//   routes: {
+//     // '/': (context) => Loading(),
+//     // '/home': (context) => Home(),
+//
+//     '/login': (context) => LoginScreen(),
+//     // '/registration': (context) => Registration(),
+//     '/profile': (context) => Profile(),
+//
+//
+//   },
+// ));
--- test/validator_test.dart
+++ test/validator_test.dart
@@ -1,20 +1,53 @@
 // import 'package:login_demo/login_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:streamer_review/helper/database_helper.dart';
+import 'package:streamer_review/helper/database_helper.dart' as DBHelper;
+import 'package:streamer_review/model/user.dart';
+
+import 'package:streamer_review/widgets/anotherMain.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
+  // TestWidgetsFlutterBinding.ensureInitialized();
+  // WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 
   test('return the database', () async {
-    TestWidgetsFlutterBinding.ensureInitialized();
+    // WidgetsFlutterBinding.ensureInitialized();
+    // TestWidgetsFlutterBinding.ensureInitialized();
+    runApp(MyApp());
 
+    DatabaseHelper2 d = DBHelper.DatabaseHelper2.instance;
+    // TestWidgetsFlutterBinding.ensureInitialized();
+    // Future<List<Map<String, dynamic>>> result = d.queryAllUsers();
+    // var result = await d.queryAllUsers();
+    Future<List<Map<String, dynamic>>> result2 = d.queryAllUsers();
+
+    print(result2.toString().length);
+    List<User> userList = [];
+    User user = new User();
+    List.generate(result2.toString().length, (i) {
+      userList.add(User(
+        // id: user[i]['_id'],
+        // email: user[i]['email'],
+        // password: user[i]['password'],
+        // userName: user[i]['user_name'],
+        // phoneNumber: user[i]['phone_number'],
+      ));
+    });
+   print(userList.length);
+    int id = userList.first.id;
+    print(id);
+    // for(var x in result2){
+    //   print(x);
+    // }
+    print(result2);
     // var db = await openDatabase('myDatabase24.db');
 
     // final result = EmailFieldValidator.validate('');
     // expect(result, 'Email can\'t be empty');
-    // Database db2 = await DatabaseHelper2.instance.database;
+    // Database db2 = DatabaseHelper2.instance;
     // List<Map> result = await db.rawQuery(
     //     'SELECT * FROM broadcaster_table WHERE broadcaster_id=?',
     //     [2224]);
@@ -22,6 +55,8 @@ void main() {
   });
 
   test('non-empty email returns null', () async {
+
+
 
     var db = await openDatabase('myDatabase24.db');
 
