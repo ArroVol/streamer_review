d9c389cb8791214459a7712d76734d483d3bf3b7
Brendamus <50035059+Brendamus@users.noreply.github.com>
2020-12-07 19:35:39 -0600

    Linked customize home page settings to the home page. The user is now able to adjust what they see on the home page through the settings>customize.
    To-do still waiting on connection to db.

--- lib/category.dart
+++ lib/category.dart
@@ -1,9 +1,33 @@
 class Category {
   String category;
   bool selected;
+  int position;
+
+  // Category(String category, bool selected){
+  //   this.category = category;
+  //   this.selected = selected;
+  // }
+
+  Category.forJson({
+   this.category,
+   this.selected,
+});
 
   Category(String category, bool selected){
     this.category = category;
     this.selected = selected;
+
   }
+
+  factory Category.fromJson(Map<String, dynamic> jsonData) {
+    return Category.forJson(
+      category: jsonData['category'],
+      selected: jsonData['selected'],
+    );
+  }
+
+  static Map<String, dynamic> toMap(Category category) => {
+    'category': category.category,
+    'selected': category.selected,
+  };
 }
--- lib/customize_home.dart
+++ lib/customize_home.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:streamer_review/streamer_screen.dart';
 
 import 'category.dart';
 
@@ -23,6 +28,13 @@ class _CustomizeHomeState extends State<CustomizeHome> {
     new Category('Beauty & Body Art', true)
   ];
 
+  List<SwitchListTile> categoryList = new List<SwitchListTile>();
+
+  @override
+  void initState() {
+    getList().then(updateCategories);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,12 +48,17 @@ class _CustomizeHomeState extends State<CustomizeHome> {
         ),
         backgroundColor: Colors.black54,
       ),
-      body: ReorderableListView(onReorder: onReorder, children: getListItems()),
-        backgroundColor: Colors.black45,
+      body: ReorderableListView(onReorder: onReorder, children: categoryList),
+      backgroundColor: Colors.black45,
     );
   }
 
-  List<SwitchListTile> getListItems() => categories
+  // List<SwitchListTile> getListItems()  {
+  //   getList().then(updateList())
+  //   return buildList();
+  // }
+
+  List<SwitchListTile> buildList() => categories
       .asMap()
       .map((index, item) => MapEntry(index, buildTenableListTile(item, index)))
       .values
@@ -62,6 +79,7 @@ class _CustomizeHomeState extends State<CustomizeHome> {
           setState(() {
             category.selected = value;
           });
+          refresh();
         },
       );
 
@@ -71,9 +89,46 @@ class _CustomizeHomeState extends State<CustomizeHome> {
     }
     setState(() {
       Category category = categories[oldIndex];
-
       categories.removeAt(oldIndex);
       categories.insert(newIndex, category);
     });
+    refresh();
+  }
+
+  static String encode(List<Category> listOfCategories) => json.encode(
+        listOfCategories
+            .map<Map<String, dynamic>>((category) => Category.toMap(category))
+            .toList(),
+      );
+
+  static List<Category> decode(String toDecodeCategories) =>
+      (json.decode(toDecodeCategories) as List<dynamic>)
+          .map<Category>((item) => Category.fromJson(item))
+          .toList();
+
+  Future<bool> saveList() async {
+    print('Saving Categories');
+    String encodedCategories = encode(categories);
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setString('categories', encodedCategories);
+  }
+
+  Future<String> getList() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getString('categories');
+  }
+
+  void updateCategories(String categoriesString) {
+    setState(() {
+      print(categoriesString);
+      this.categories = decode(categoriesString);
+      categoryList = buildList();
+      print('here');
+    });
+  }
+
+  void refresh() {
+    saveList();
+    getList().then(updateCategories);
   }
 }
